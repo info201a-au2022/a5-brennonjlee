5b69d3f57a8b4c444cf7fa72b8e26cb8bdd15573
brennon <brennonjlee@gmail.com>
2022-12-08 20:01:33 -0800

    update

--- app_server.R
+++ app_server.R
@@ -14,7 +14,74 @@ library(maps)
 data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
 
 server <- function(input, output) {
-  
+  ### Chart
+  output$chart <- renderPlotly({
+    if(input$inp_var == "co2") {
+      #filters data by the state input
+      chart_country_data <- data %>%
+        filter(country == input$inp) %>%
+        group_by(year)
+      
+      #creates the line chart
+      chart <- chart_country_data %>%
+        ggplot(mapping = aes_string(x = "year", y = "co2")) +
+        geom_line() +
+        geom_point() +
+        labs(
+          title = "Co2 Emissions count by Year",
+          x = "Year",
+          y = "Co2 Emissions Count"
+        ) 
+      } else if (input$inp_var == "population") {
+        #filters data by the state input
+        chart_country_data <- data %>%
+          filter(country == input$inp) %>%
+          group_by(year)
+        
+        #creates the line chart
+        chart <- chart_country_data %>%
+          ggplot(mapping = aes_string(x = "year", y = "population")) +
+          geom_line() +
+          geom_point() +
+          labs(
+            title = "Population by Year",
+            x = "Year",
+            y = "Population"
+          ) 
+      } else if (input$inp_var == "co2_per_capita") {
+        #filters data by the state input
+        chart_country_data <- data %>%
+          filter(country == input$inp) %>%
+          group_by(year)
+        
+        #creates the line chart
+        chart <- chart_country_data %>%
+          ggplot(mapping = aes_string(x = "year", y = "co2_per_capita")) +
+          geom_line() +
+          geom_point() +
+          labs(
+            title = "Co2 Per Capita by Year",
+            x = "Year",
+            y = "Co2 Per Capita"
+          ) 
+      } else if (input$inp_var == "co2_per_gdp") {
+        #filters data by the state input
+        chart_country_data <- data %>%
+          filter(country == input$inp) %>%
+          group_by(year)
+        
+        #creates the line chart
+        chart <- chart_country_data %>%
+          ggplot(mapping = aes_string(x = "year", y = "co2_per_gdp")) +
+          geom_line() +
+          geom_point() +
+          labs(
+            title = "Co2 Per GDP by Year",
+            x = "Year",
+            y = "Co2 Per GDP"
+          ) 
+      }
+  })
 }
 
 
--- app_ui.R
+++ app_ui.R
@@ -27,8 +27,12 @@ max_co2_2021_country <- co2_data_2021 %>%
   filter(co2 == max(co2)) %>%
   pull(country)
 
-min_co2_2021 = min(co2_data_2021$co2)
+min_co2_2021 <- co2_data_2021 %>%
+  filter(co2 != 0) %>%
+  filter(co2 == min(co2)) %>%
+  pull(co2)
 min_co2_2021_country <- co2_data_2021 %>%
+  filter(co2 != 0) %>%
   filter(co2 == min(co2)) %>%
   pull(country)
 
@@ -50,16 +54,61 @@ intro_panel <- tabPanel(
     h3("Key Values"),
     p("Diving deeper into the dataset, I noticed that the most recent data that was collected was in 2021. As a result, 
       across the entire dataset, the highest co2 value was in 2021 at", max_co2_2021, " million tonnes. This was the 
-      co2 emissions value of the entire ", max_co2_2021_country, ". In contrast, the lowest co2 value in 2021 was ",
+      co2 emissions value of the entire ", max_co2_2021_country, ". In contrast, the lowest co2 value in 2021 above 0 was ",
       min_co2_2021, " million tonnes in the country ", min_co2_2021_country, ". As a comparison, across the entire 
       world in 2021, the average co2 emissions was ", avg_co2_2021, " million tonnes. While these numbers are helpful,
       they also don't really mean anything if there isn't a number to compare it to. So, over the last 10 years, the co2
       emissions has increased by about ", co2_change_10_yr, " million tonnes. This is alarming as you can see that co2
-      emissions have definitely been on the rise in pretty large quantities.")
+      emissions have definitely been on the rise in pretty large quantities."),
+    h3("Aims"),
+    p("Through the creation of these charts I am to find patterns that could explain co2 emissions and how they have changed
+      over the years. This report will be exploring how co2, population, co2 per capita, and co2 per gdp have changed
+      over the years and what patterns can be found based off these charts. ")
+  )
+)
+
+chart_var_input <- selectInput(
+  inputId = "inp_var",
+  label = "Select a variable",
+  choice = c("co2",
+             "population",
+             "co2_per_capita",
+             "co2_per_gdp"),
+  selected = "co2"
+)
+
+chart_country_input <- selectInput(
+  inputId = "inp",
+  label = "Select a country",
+  choice = unique(co2_data["country"]),
+  selected = "World"
+)
+
+chart_panel <- tabPanel(
+  "Interactive Visuals",
+  titlePanel("Interactive Visuals From the Data"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Please select an input variable and an input country to view the data."),
+      chart_var_input,
+      chart_country_input
+    ),
+    mainPanel(
+      plotlyOutput("chart"),
+      h3("Why this chart?"),
+      p("These charts show the data over the years until 2021 for every country. Through these charts you are able to visually see
+        how the co2 emissions, population, co2 per capita, and co2 per gdp have risen or fallen for each country. As shown, the 
+        co2, population, and co2 per capita have risen pretty drastically. Starting around 1950, these numbers start to increase at a 
+        drastic rate. However, for co2 per gdp has risen drastically until about 1940 where it has started to drop. Basesd off these graphs
+        we can tell that for while population has been rising, the co2 per each person has risen even more, yet in recent years, the co2 per 
+        dollar of a country hasn't risen as much. This is still pretty alarming as if something doesn't happen, co2 will continue to rise 
+        drastically at these rates.")
+    )
   )
 )
 
 ui <- navbarPage(
   "Co2 Emissions",
-  intro_panel
+  intro_panel,
+  chart_panel
 )
